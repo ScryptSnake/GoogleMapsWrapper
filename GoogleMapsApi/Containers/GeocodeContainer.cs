1f83bfc2df766f926cff84691207ca941980bc64
ScryptSnake <164273150+ScryptSnake@users.noreply.github.com>
2024-10-18 09:13:42 -0400

    Update GeocodeContainer.cs
    Added summary comments.

--- GoogleMapsApi/Containers/GeocodeContainer.cs
+++ GoogleMapsApi/Containers/GeocodeContainer.cs
@@ -13,32 +13,33 @@ using System.Xml;
 
 
 namespace GoogleMapsWrapper.Containers;
-
+/// <summary>A container to hold parsed JSON information from a GoecodeAPI response.</summary>
 public record GeocodeContainer(
+    /// <summary>The 'Country' parsed from the response. This property is set from the API's 'country' JSON property</summary>
     [JsonProperty("country")]
     string Country,
-
+    /// <summary>The 'State' parsed from the response. This property is set from the API's 'administrative_area_level_1' JSON property.</summary>
     [JsonProperty("administrative_area_level_1")]
     string State,
-
+    /// <summary>The 'County' parsed from the response. This property is set from the API's 'administrative_area_level_2' JSON property.</summary>
     [JsonProperty("administrative_area_level_2")]
     string County,
-
+    /// <summary>The 'City' parsed from the response. This property is set from the API's 'locality' JSON property.</summary>
     [JsonProperty("locality")]
     string City,
-
+    /// <summary>The 'Municipality' parsed from the response. This property is set from the API's 'administrative_area_level_2' JSON property.</summary>
     [JsonProperty("administrative_area_level_3")]
     string Municipality,
-
+    /// <summary>The 'ZipCode' parsed from the response. This property is set from the API's 'postal_code' JSON property.</summary>
     [JsonProperty("postal_code")]
     int ZipCode,
-
+    /// <summary>The 'Address' parsed from the response. This property is set from the API's 'formatted_address' JSON property.</summary>
     [JsonProperty("formatted_address")]
     string Address,
 
-    //this property is not de-serialized.but manually added in GeocodeResponse.
+    /// <summary>The Gps Coordinate the request targets.</summary>
     GpsCoordinate? Coordinates,
-
+    /// <summary>Associated data related to the request. This contains the IRequest object.</summary>
    object? AssociatedData
 
     ) : IContainer
