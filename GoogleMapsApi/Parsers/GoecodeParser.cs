1c55602bb2776c01d56ba731bfad4a38f600286d
ScryptSnake <164273150+ScryptSnake@users.noreply.github.com>
2024-10-21 08:47:54 -0400

    Update GoecodeParser.cs
    Added summaries.

--- GoogleMapsApi/Parsers/GoecodeParser.cs
+++ GoogleMapsApi/Parsers/GoecodeParser.cs
@@ -10,8 +10,10 @@ using System.Threading.Tasks;
 
 
 namespace GoogleMapsWrapper.Parsers;
+///<Summary>A parser for an Elevation API JSON response. Parses data and outputs an ElevationContainer.</Summary>
 public class GeocodeParser : IParser<GeocodeContainer, JsonDocument>
 {
+    ///<Summary>Attempts to parse a JsonDocument.</Summary>
     public bool TryParse(JsonDocument input, out GeocodeContainer? output)
     {
         try
@@ -25,39 +27,39 @@ public class GeocodeParser : IParser<GeocodeContainer, JsonDocument>
             return false;
         }
     }
-
+    ///<Summary>Parses a JsonDocument from a GeocodeResponse to a container.</Summary>
     public GeocodeContainer Parse(JsonDocument input)
     {
-        //create a dictionary to hold retrieved data
+        // Validate input.
         if (input == null)
             throw new NullReferenceException("A JSON Response was not found.");
 
-        //Grab 'results' array from response
+        // Grab 'results' array from the response.
         JsonElement results = input.RootElement.GetProperty("results");
 
-        // Get second item in result, ignore first and others
+        // Validate length of 'results' array.
         if (results.GetArrayLength() >= 2)
         {
-            // Access the second result - it is unclear from the web API what the other results are describing. Partial info?
+            // Access the second result from 'results' - It is unclear from the web API what the other results are describing. Partial info?
             JsonElement secondResult = results[1];
 
-            // find address_components array item in second result, the array contains the bulk of info needed.
+            // Find 'address_components' array item in second result, the array contains the bulk of info needed.
+            // Then, pass to method to parse the array to a dict.
             var data = ParseAddressComponents(secondResult.GetProperty("address_components"));
-
-            //grab formatted_address property and add to dict.
+            
+            // Grab formatted_address property and add to dict.
             var address = secondResult.GetProperty("formatted_address").GetString();
             data.Add("formatted_address", address ?? string.Empty);
 
-            //Serialize back to Json...
+            // Serialize dict back to Json.
             var json = JsonConvert.SerializeObject(data, Formatting.Indented);
 
-            //Deserialize into result object
+            // Deserialize into result object.
             var container = JsonConvert.DeserializeObject<GeocodeContainer>(json);
-
             if (container == null) 
                 throw new System.Text.Json.JsonException("Failed to derserialize geocode response");
 
-            // Find coordinates and create new container with coords added...
+            // Find coordinates and create a copy of container with coordinates added.
             var containerWithCoordinates = container with
             {
                 Coordinates = FindGpsCoordinate(secondResult.GetProperty("geometry").GetProperty("location")),
@@ -70,23 +72,27 @@ public class GeocodeParser : IParser<GeocodeContainer, JsonDocument>
             throw new System.Text.Json.JsonException("Expected 2 results in address components.");
         }
     }
+
+
+    ///<Summary>Parses the 'address_components' JSON array to a Dictionary.
+    ///<para>Each value in the argument contains 3 properties: 'long_name', 'short_name', 'types[]'.</para>
+    ///<para>This method acquires the 'long_name' (=value), and the first element in the 'types' array (=key).</para>
+    ///</Summary>
     private Dictionary<string, string?> ParseAddressComponents(JsonElement jsonElement)
     {
-        //assembles a KVP of address related properties from the 'address_components' JSON array.
-        //The array contains 3 properties:  long_name, short_name, types[].
-        //We are interested in the long_name property (= key) and the first element in the types[] array ( = value)
+        // Establish a new Dict for output.
         var output = new Dictionary<string, string?>();
         {
-            //loop through address_components array
+            // Loop through the JsonElement.
             foreach (var arrayElement in jsonElement.EnumerateArray())
             {
-                //First item in "types" is 'property name' or key.
+                // First item in "types[]" is 'property name' or key.
                 string? typeName = arrayElement.GetProperty("types")[0].GetString();
                 if (string.IsNullOrEmpty(typeName))
                     throw new System.Text.Json.JsonException($"Type name for address component is empty.");
-                //long_name property is the 'property value'
+                // 'long_name' property is the value of interest.
                 var typeValue = arrayElement.GetProperty("long_name").GetString();
-                //append address component to dictionary
+                // Append the KVP to dictionary.
                 output.Add(typeName, typeValue);
             }
             return output;
@@ -94,10 +100,9 @@ public class GeocodeParser : IParser<GeocodeContainer, JsonDocument>
         //output.ToList().ForEach(kvp => Debug.Print($"{kvp.Key}: {kvp.Value}"));
     }
 
+    ///<Summary>Finds the GpsCoordinate within a JsonElement.</Summary>
     private GpsCoordinate FindGpsCoordinate(JsonElement jsonElement)
     {
-        //find gps Coordinate in Json result. 
-        // Used by the Parse routine.
         try
         {
             decimal latitude = 0;
