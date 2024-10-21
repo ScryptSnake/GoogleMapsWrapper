a2146d6f44d50932c2737863d935b20b4b004e92
ScryptSnake <164273150+ScryptSnake@users.noreply.github.com>
2024-10-21 08:55:00 -0400

    Update IRequest.cs
    Added summary.

--- GoogleMapsApi/Requests/IRequest.cs
+++ GoogleMapsApi/Requests/IRequest.cs
@@ -10,10 +10,11 @@ using System.Threading.Tasks;
 
 
 namespace GoogleMapsWrapper.Requests;
+///<Summary>A type that represents a request to be sent to the engine for HTTP sending.</Summary>
 public interface IRequest
 {
     public string? Id { get; }
-    public Uri Url { get; } //note:  Url should not ever contain the API Key!, this is done in the engine and never exposed in a returned reponse.
+    public Uri Url { get; }
     public string Query { get; }
     public ApiTypes Api { get; }
     public RequestTypes Category { get; }
